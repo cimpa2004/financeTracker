cd040e00260f080f608e19f90bfb436edbc323a4
Dézsenyi Balázs <bz.dezsenyi@gmail.com>
2025-10-09 13:50:23 +0200

    🐞 fix: Logosize

--- frontend/src/assets/Logo.tsx
+++ frontend/src/assets/Logo.tsx
@@ -8,7 +8,7 @@ interface MainLogoProps {
 
 export default function MainLogo({ width, height }: MainLogoProps) {
   return (
-    <Box width={width || 200} height={height || 200}>
+    <Box width={width || 300} height={height || 300}>
       <img src={Logo} alt="App Logo" style={{ width: '100%', height: '100%' }} />
     </Box>
   );
