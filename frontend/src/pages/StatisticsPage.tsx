8fb5bdc7b0ff7b35d522e1d91ee50a6275b54cc9
Dézsenyi Balázs <bz.dezsenyi@gmail.com>
2025-10-25 15:31:27 +0200

    🐞 fix: statisctics page pinterest stlye grid

--- frontend/src/pages/StatisticsPage.tsx
+++ frontend/src/pages/StatisticsPage.tsx
@@ -1,19 +1,19 @@
 import { Box } from "@mui/material";
 import SpentByCategoryCard from "../components/SpentByCategoryCard";
 import SpentByIntervalCard from "../components/SpentByIntervalCard";
+import Masonry from '@mui/lab/Masonry';
 
 export default function StatisticsPage() {
   return (
-    <Box p={2} width="100vw" justifyContent="center" alignItems="center">
-      <Box
-        display="grid"
-        gridTemplateColumns={{ xs: '1fr', sm: '1fr 1fr', md: '1fr 1fr' }}
-        gap={2}
-      >
-        <SpentByCategoryCard />
-        <SpentByIntervalCard />
-      </Box>
-
+    <Box p={2} width="100%" sx={{ boxSizing: 'border-box' }}>
+      <Masonry columns={{ xs: 1, sm: 2, md: 2 }} spacing={2}>
+        <Box>
+          <SpentByCategoryCard />
+        </Box>
+        <Box>
+          <SpentByIntervalCard />
+        </Box>
+      </Masonry>
     </Box>
   );
 }
